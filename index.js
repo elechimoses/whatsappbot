a0d76d375d7724a2bb095aed95f66c6666b45d6b
mosesgolden <moses@onepipe.io>
2025-07-12 21:36:57 +0100

    index route

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connecte
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', (req, res) => {
+    res.send({title: 'Welcome to bot'})
+})
 app.use('/whatsapp', whatsappRoutes);
 
 const PORT = process.env.PORT || 3000;
